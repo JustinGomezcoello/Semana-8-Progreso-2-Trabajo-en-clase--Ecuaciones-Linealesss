e4b582e628bb32defea5684b1fb93681eaffcf43
JustinGomezcoello <cristian.gomezcoello@udla.edu.ec>
2023-06-01 21:40:09 -0500

    ecuaciones lineales

--- main.c
+++ main.c
@@ -8,40 +8,40 @@ int main(int argc, char *argv[])
   float A[3][3];
   float B[3][1];
 
-  printf("Ingrese el priemer valor de X\n");
+  printf("Ingrese el primer valor de X\n");
   scanf("%f", &A[0][0]);
 
-  printf("Ingrese el priemer valor de Y\n");
+  printf("Ingrese el primer valor de Y\n");
   scanf("%f", &A[0][1]);
 
-  printf("Ingrese el priemer valor de Z\n");
+  printf("Ingrese el primer valor de Z\n");
   scanf("%f", &A[0][2]);
 
-  printf("Ingrese el priemer valor de igualdad de la primera ecuacion\n");
+  printf("Ingrese el primer valor de igualdad de la primera ecuacion\n");
   scanf("%f", &B[0][0]);
 
-  printf("Ingrese el priemer valor de X\n");
+  printf("Ingrese el segundo valor de X\n");
   scanf("%f", &A[1][0]);
 
-  printf("Ingrese el priemer valor de Y\n");
+  printf("Ingrese el segundo valor de Y\n");
   scanf("%f", &A[1][1]);
 
-  printf("Ingrese el priemer valor de Z\n");
+  printf("Ingrese el segundo valor de Z\n");
   scanf("%f", &A[1][2]);
 
-  printf("Ingrese el priemer valor de igualdad de la primera ecuacion\n");
+  printf("Ingrese el segundo valor de igualdad de la primera ecuacion\n");
   scanf("%f", &B[1][0]);
 
-  printf("Ingrese el priemer valor de X\n");
+  printf("Ingrese el tercer valor de X\n");
   scanf("%f", &A[2][0]);
 
-  printf("Ingrese el priemer valor de Y\n");
+  printf("Ingrese el tercer valor de Y\n");
   scanf("%f", &A[2][1]);
 
-  printf("Ingrese el priemer valor de Z\n");
+  printf("Ingrese el tercer valor de Z\n");
   scanf("%f", &A[2][2]);
 
-  printf("Ingrese el priemer valor de igualdad de la primera ecuacion\n");
+  printf("Ingrese el tercer valor de igualdad de la primera ecuacion\n");
   scanf("%f", &B[2][0]);
   calculos( A,B);
   return 0;
